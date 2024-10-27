52cfc22d0e4202ac0d791847e6b5208a46c09f36
TaraAlcira <tarasplint@icloud.com>
2024-10-27 12:44:22 +0100

    update 1

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,15 +1,18 @@
 const menu = document.getElementById('menu');
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger-container');
 
 hamburger.addEventListener('click', () => {
-    console.log("Hamburger clicked!"); // Check this message in the console
+    const hamburgerIcon = document.querySelector('.hamburger');
+    // Toggle the active class on both hamburger and menu
+    hamburgerIcon.classList.toggle('active');
     menu.classList.toggle('active');
 });
-
 
 document.addEventListener('click', function(event) {
     // Close the menu if you click outside the menu or the hamburger
     if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
+        const hamburgerIcon = document.querySelector('.hamburger');
         menu.classList.remove('active'); // Close the menu
+        hamburgerIcon.classList.remove('active'); // Reset the hamburger to default
     }
 });
--- assets/js/slinger.js
+++ assets/js/slinger.js
@@ -1,28 +1,27 @@
 const container = document.querySelector('.slinger');  
 const images = container.querySelectorAll('img');  
 
-let lastScrollPosition = 0;  
-  
+let lastScrollPosition = 0; 
+
+// start position is width of screen
+images.forEach((image, index) => {  
+  image.style.transform = `translateX(${index * window.innerWidth}px)`;  
+});
+
+// Move the images horizontally as the user scrolls vertically
 window.addEventListener('scroll', () => {  
   const scrollPosition = window.scrollY;  
+  // const scrollPositionDelta = scrollPosition - lastScrollPosition;
   const containerHeight = container.offsetHeight;  
   const imageWidth = images[0].offsetWidth;  
   
   // Calculate the horizontal scroll position based on the vertical scroll position  
   const horizontalScrollPosition = (scrollPosition / containerHeight) * imageWidth * 0.2; // Slow down the movement by multiplying by 0.2  
   
-  // Check if the user is scrolling up or down  
-  if (scrollPosition < lastScrollPosition) {  
-   // User is scrolling up, move the images back to the left  
-   images.forEach((image, index) => {  
-    image.style.transform = `translateX(${horizontalScrollPosition - (index * imageWidth)}px)`;  
-   });  
-  } else {  
-   // User is scrolling down, move the images to the right  
-   images.forEach((image, index) => {  
+  // Adjust the position of the images as the user scrolls
+  images.forEach((image, index) => {  
     image.style.transform = `translateX(${horizontalScrollPosition + (index * imageWidth)}px)`;  
-   });  
-  }  
-  
+  });
+
   lastScrollPosition = scrollPosition;  
 });
