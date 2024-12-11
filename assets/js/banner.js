be5062ba9dbf6d2410b4f5ab0c46e7849f74eb25
TaraAlcira <tarasplint@icloud.com>
2024-12-11 10:54:13 +1000

    fixing index page

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,19 +1,25 @@
-const videoSource = document.getElementById('video-source');
+function updateBannerForScreenSize() {
+    const iframeContainer = document.getElementById("iframe-container");
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-function updateVideoSource() {
-    if (window.innerWidth <= 768) {
-        // Mobile version
-        videoSource.src = 'images/vid_mobile_logo.mp4'; // Change to your mobile video file
+    // Replace iframe content based on screen size
+    if (isMobile) {
+        iframeContainer.innerHTML = `
+            <iframe src="https://player.vimeo.com/video/1037793710?badge=0&autopause=0&player_id=0&app_id=58479/embed"
+                allow="autoplay; fullscreen; picture-in-picture" allowfullscreen frameborder="0"
+                style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+        `;
+        iframeContainer.style.padding = "177.78% 0 0 0";
     } else {
-        // Desktop version
-        videoSource.src = 'images/vid_logo.mp4'; // Change to your desktop video file
+        iframeContainer.innerHTML = `
+            <iframe src="https://player.vimeo.com/video/1022964755?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?autoplay=1&loop=1&background=1"
+                frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+                style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+        `;
+        iframeContainer.style.padding = "56.25% 0 0 0";
     }
-    // Load the new video source
-    const video = document.getElementById('banner-video');
-    video.load();
 }
 
-// Check on page load
-updateVideoSource();
-// Check on resize
-window.addEventListener('resize', updateVideoSource);
+// Run on load and resize
+updateBannerForScreenSize();
+window.addEventListener("resize", updateBannerForScreenSize);
