fbe29ac0eb21136b445d314a268585290414a9a7
TaraAlcira <tarasplint@icloud.com>
2024-12-16 19:57:33 +1000

    veranderingen 16.12

--- assets/js/slinger.js
+++ assets/js/slinger.js
@@ -4,7 +4,7 @@ const images = container.querySelectorAll('img');
 let lastScrollPosition = 0;
 const imageWidth = images[0].offsetWidth;
 
-const startOffset = window.innerWidth * 1.2; 
+const startOffset = window.innerWidth * 0.8; 
 
 // Set the initial position of the images to start off-screen to the right, but with the adjusted offset
 images.forEach((image, index) => {
